da4625d43d5e2e01e4c28cf0c8a4451c173379cb
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-11-20 19:42:46 +0300

    feat: add primitive data types

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + (i +1));
+            System.out.println("i = " + (i + 2));
         }
+//        Primitive data types
+        int number1 = 5;
+//        byte by = 127;
+//        short sh = 558;
+//        long L = 58000L;
+//
+//        float number = 3.4F;
+//        double d = 3.4;
+
+        char c = 'i';
+        c++;
+//        boolean isOkay = true;
+        System.out.println("number1 = " + number1);
+        System.out.println("char c = " + c);
+
     }
 }
