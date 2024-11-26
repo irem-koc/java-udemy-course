21b43bb9db1e628ea548e061ff536169252bd8a8
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-11-26 21:42:32 +0300

    feat: add logical operators

---
+++ src/LogicalOperators.java
@@ -0,0 +1,11 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+//        int x = 7;
+//        int y = 5;
+//        int a = 5;
+//        int b = 10;
+//        boolean result = (x > y) && (a != b);
+//        System.out.println("result = " + result);
+
+    }
+}
