855dd9db9bcf9cccf806a2b678e5e33eb2140821
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-06 21:08:13 +0300

    feat: add method overloaded

--- src/ObjectPracticals.java
+++ src/ObjectPracticals.java
@@ -1,8 +1,12 @@
 class Calculator{
-    int a;
     public int add(int a, int b){
         System.out.println("adding..");
         return a+b;
+    }
+    //Method Overloading
+    public int add(int a, int b, int c){
+        System.out.println("adding..");
+        return a+b+c;
     }
 }
 public class ObjectPracticals {
@@ -13,5 +17,9 @@ public class ObjectPracticals {
         Calculator calculator = new Calculator();
         int result = calculator.add(num1, num2);
         System.out.println(result + " is the result of add function");
+        int num3 = 5;
+
+        int result2 = calculator.add(num1, num2, num3);
+        System.out.println(result2 + " is the result of add function");
     }
 }
