4d1703ac2c0ee3d900b753ead84413c753386484
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-12 21:00:24 +0300

    feat: add some naming conventions

---
+++ src/NamingConventions.java
@@ -0,0 +1,10 @@
+public class NamingConventions {
+    public static void main(String[] args) {
+        //Camel casing
+
+        //class and interfaces -> Calc, ..
+        //Variable and method mark show(), showMyMask()
+        // constants -> PIE, PIE_AND_BREAD
+
+    }
+}
