02b9ec1ba7489361cf6af9783c86ffb95a604d70
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-24 00:47:36 +0300

    feat: inner class and anonymous inner class added

---
+++ src/InnerClass/Main.java
@@ -0,0 +1,31 @@
+package InnerClass;
+class A{ //not here static
+    int age;
+    public void show(){ //non static method
+        System.out.println("");
+    }
+    static class B{ //only inner class
+        public void config(){
+            System.out.println("inner worked");
+        }
+    }
+}
+class C {
+    public void show(){
+        System.out.println("in show method");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        A.B b = new A.B();
+
+        C c = new C(){ //with this we are creating this anonymous class not C
+            //anonymous inner class
+            public void show(){
+                System.out.println("c inner worked");
+            }
+        };
+        c.show();
+    }
+}
