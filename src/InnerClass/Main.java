53e06005be6a1f84a4637fb93b63b7d1b8ac0a02
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-09 02:29:52 +0300

    feat: add details

--- src/InnerClass/Main.java
+++ src/InnerClass/Main.java
@@ -4,7 +4,7 @@ class A{ //not here static
     public void show(){ //non static method
         System.out.println("");
     }
-    static class B{ //only inner class
+    static class B{ //only inner class -- COMPOSİTİON AND AGGREGATİON
         public void config(){
             System.out.println("inner worked");
         }
@@ -19,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
         A a = new A();
         A.B b = new A.B();
-
+        b.config();
         C c = new C(){ //with this we are creating this anonymous class not C
             //anonymous inner class
             public void show(){
